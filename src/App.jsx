41ad1e60fe532a6455002d62c615172b8a576baa
Bon Aguilan <aguilanbon@gmail.com>
2023-01-20 20:59:48 +0800

    added react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const getItem = async () => {
+    const result = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    if (result.status === 200) {
+      const json = result.data;
+      console.log(json);
+    }
+  };
 
-  // const getItem = async () => {
-  //   const result = await 
-  // }
-
-  // useEffect(() => {
-
-  // },[])
+  useEffect(() => {
+    getItem();
+  }, []);
 
   return (
     <div className="w-screen min-h-screen">
-      <button className='btn btn-warning'>Click me</button>
+      <button className="btn btn-warning">Click me</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
