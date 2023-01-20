7d3189f1707a5595d05a31da4900d53040817899
Bon Aguilan <aguilanbon@gmail.com>
2023-01-20 20:43:58 +0800

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 function App() {
 
+  // const getItem = async () => {
+  //   const result = await 
+  // }
+
+  // useEffect(() => {
+
+  // },[])
+
   return (
-    <div className="App">
+    <div className="w-screen min-h-screen">
       <button className='btn btn-warning'>Click me</button>
     </div>
   )
