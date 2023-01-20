96fbc4a653f81ffb441fc4d4f088240bfb14d4bb
Bon Aguilan <aguilanbon@gmail.com>
2023-01-20 20:35:39 +0800

    added daisy ui component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className='text-red-500'>hi</h1>
-      <h1 className='text-green-500'>heloos</h1>
+      <button className='btn btn-warning'>Click me</button>
     </div>
   )
 }
